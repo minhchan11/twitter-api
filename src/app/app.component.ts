4a24074cf1f916a8d58a6a087074941a82cd8bf8
Minh Phuong <mphuong@kent.edu>
2017-04-11 16:32:44 -0700

    add news feed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Headers} from '@angular/http';
+import { FeedService } from './feed-service.service';
+
+// Add the RxJS Observable operators we need in this app.
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[FeedService]
 })
 export class AppComponent {
+  private feedUrl: string = 'http%3A%2F%2Fdata.unhcr.org%2Fsyrianrefugees%2Frss%2Fregional.php%3FLang%3DEN%26Instance%3Dsyria';
+  private feeds: any;
   title = 'app works!';
   tweetsdata;
 
-  constructor(private http: Http){}
+  constructor(private http: Http, private feedService: FeedService){}
   makecall() {
     var headers = new Headers();
 
@@ -32,10 +39,19 @@ export class AppComponent {
     });
   }
 
+refreshFeed() {
+    this.feedService.getFeedContent(this.feedUrl)
+        .subscribe(feed => { console.log(feed['items']);this.feeds = feed['items']}, 
+        error => console.log(error))
+  }
+
+
   ngOnInit() {
     this.makecall();
 
     this.searchcall();
+
+     this.refreshFeed();
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { StripHtmlTagsPipe } from './pipe/strip-html-tags.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    StripHtmlTagsPipe
   ],
   imports: [
     BrowserModule,
